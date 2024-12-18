6dd1fa4e599e4703268e5ba8d4d1a201fd189f20
Wildor JUlca <140985554+wildorjulca@users.noreply.github.com>
2024-12-18 18:23:14 -0500

    new cambios

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,7 @@
 
-
-import path from "path"
-import react from "@vitejs/plugin-react-swc"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
@@ -11,4 +10,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
